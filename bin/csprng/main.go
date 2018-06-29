02e1902607b70cce068eba583afef5f1781bfc89
Lukas Jarosch <lukas.jarosch@mail.com>
2018-06-29 23:37:01 +0200

    Add a cryptographically secure PRNG token generator
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

---
+++ bin/csprng/main.go
@@ -0,0 +1,45 @@
+package main
+import(
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+// Credit goes to: Matt Silverlock <https://blog.questionable.services>
+
+
+// GenerateRandomBytes returns securely generated random bytes.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+    // Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// GenerateRandomString returns a URL-safe, base64 encoded
+// securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
+}
+
+func main() {
+	fmt.Println("+---------------------------------------------------------------+")
+	fmt.Println("| CSRNG - Cryptographically Safe Pseudo Random Number Generator |")
+	fmt.Println("+---------------------------------------------------------------+\n")
+	token, err := GenerateRandomString(64)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Token:", token)
+}
+
