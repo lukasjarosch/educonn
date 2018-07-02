faf9403284a99aa48c3070e87cb022ea798bec5e
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-03 00:46:21 +0200

    Add docker deployment
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ travis:
 	cd srv/mail/ && $(MAKE) travis && cd ../..
 	cd srv/user/ && $(MAKE) travis && cd ../..
 
+travis-deploy:
+	cd srv/mail/ && $(MAKE) travis-deploy && cd ../..
+	cd srv/user/ && $(MAKE) travis-deploy && cd ../..
---
+++ bin/deploy-travis.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+DOCKER_HOST="registry-1.docker.io"
+
+mkdir -p dist
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD $DOCKER_HOST
+export REPO=$DOCKER_USER/educonn-mail
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_TAG ; fi`
+echo $REPO:$TAG
+docker build -f Dockerfile -t $REPO:$TAG .
+docker push $REPO
