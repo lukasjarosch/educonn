e560d216bb12cf0fbac9dfa892ede71b5438cfd2
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-03 01:08:17 +0200

    adjusting..
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- bin/deploy-travis.sh
+++ bin/deploy-travis.sh
@@ -2,7 +2,7 @@
 mkdir -p dist
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD $DOCKER_HOSTNAME
 export REPO=$DOCKER_USER/educonn-mail
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_TAG ; fi`
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; elif [ "$TRAVIS_BRANCH" == "develop" ]; then echo "dev"; else echo $TRAVIS_TAG ; fi`
 echo $REPO:$TAG
 docker build -f Dockerfile -t $REPO:$TAG .
 docker push $REPO
