6505e41834cc551cbd956eef6b29f80432fbca4c
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-02 22:48:28 +0200

    Wanna use protoc3 instead of 2, haha
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- bin/install-protobuf.sh
+++ bin/install-protobuf.sh
@@ -2,9 +2,9 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://github.com/google/protobuf/releases/download/v2.6.0/protobuf-2.6.0.tar.gz
-  tar -xzvf protobuf-2.6.0.tar.gz
-  cd protobuf-2.6.0 && ./configure --prefix=$HOME/protobuf && make && make install
+  wget https://github.com/google/protobuf/releases/download/v3.6.0/protobuf-cpp-3.6.0.tar.gz
+  tar -xzvf protobuf-3.6.0.tar.gz
+  cd protobuf-3.6.0 && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached directory."
 fi
