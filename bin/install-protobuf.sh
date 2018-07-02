a011bde84c338a987a8654a2dc155bb540e923df
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-02 22:06:44 +0200

    More travis config
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- bin/install-protobuf.sh
+++ bin/install-protobuf.sh
@@ -2,7 +2,7 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
+  wget https://github.com/google/protobuf/releases/download/v2.6.0/protobuf-2.6.0.tar.gz
   tar -xzvf protobuf-2.4.1.tar.gz
   cd protobuf-2.4.1 && ./configure --prefix=$HOME/protobuf && make && make install
 else
