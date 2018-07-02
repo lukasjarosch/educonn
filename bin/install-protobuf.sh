b429eab7a7063f7a96b5be8c1dad4c4853fd3b76
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-02 22:08:55 +0200

    Fix install-protobuf.sh
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- bin/install-protobuf.sh
+++ bin/install-protobuf.sh
@@ -3,8 +3,8 @@ set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/releases/download/v2.6.0/protobuf-2.6.0.tar.gz
-  tar -xzvf protobuf-2.4.1.tar.gz
-  cd protobuf-2.4.1 && ./configure --prefix=$HOME/protobuf && make && make install
+  tar -xzvf protobuf-2.6.0.tar.gz
+  cd protobuf-2.6.0 && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached directory."
 fi
