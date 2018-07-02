676224c7b59e62be943ac3df081538537e379837
Lukas Jarosch <lukas.jarosch@mail.com>
2018-07-03 01:22:54 +0200

    Every service has it's own build name..obviously
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- bin/deploy-travis.sh
+++ bin/deploy-travis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir -p dist
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD $DOCKER_HOSTNAME
-export REPO=$DOCKER_USER/educonn-mail
+export REPO=$DOCKER_USER/$1
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; elif [ "$TRAVIS_BRANCH" == "develop" ]; then echo "dev"; else echo $TRAVIS_TAG ; fi`
 echo $REPO:$TAG
 docker build -f Dockerfile -t $REPO:$TAG .
